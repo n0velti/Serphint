423ec1893c94985d01e16b59202d4100d1a0b0fd
n0velti <anuichi.e@gmail.com>
2024-11-08 14:42:18 -0500

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { Text } from 'react-native';
 
