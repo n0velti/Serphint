fe8d2248e2e51f16987bcab04f293dba8dbafad7
n0velti <anuichi.e@gmail.com>
2025-05-29 15:42:46 -0400

    save

---
+++ hooks/useLocationServices.tsx
@@ -0,0 +1,41 @@
+import * as Location from 'expo-location';
+
+
+export const useLocationServices = () => {
+    const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+    const getLocation = async () => {
+        try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            console.warn('Permission to access location was denied');
+            return null;
+        }
+    
+        const location = await Location.getCurrentPositionAsync({});
+        return location;
+        } catch (error) {
+        console.error('Error getting location:', error);
+        return null;
+        }
+    };
+
+    const getReverseGeocode = async (latitude, longitude) => {
+        console.log('Google API Key:', GOOGLE_API_KEY);
+        try {
+            console.log('Reverse geocoding coordinates:', apiKey);
+            const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_API_KEY}
+`;
+         
+            const response = await fetch(url); // this is your current response
+            const data = await response.json(); // this gives you the actual geocode result
+            console.log('Reverse geocode data:', data);
+            return data.results[0];
+        } catch (error) {
+            console.error('Error reverse geocoding:', error);
+            return null;
+        }
+    }
+    
+return { getLocation, getReverseGeocode };
+};
